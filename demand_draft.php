46b055c6b03c1c260e3019dfbc4ea1812d4800ca
Rajalakshmi Senthil <shambhavi110@gmail.com>
2015-07-17 03:27:04 +0530

    s

--- demand_draft.php
+++ demand_draft.php
@@ -6,7 +6,7 @@ $dd = $_POST["dd"];
 $sum=0;
 $date = date("Y-m-d");
 $regno ="12nmh";//Must be taken from cookies
-echo"<TABLE  class='striped'><TR><TH>Name</TH><TH>Price</TH><TH>Team Size</TH></TR>";
+echo"<div class='container row'><TABLE  class='striped'><TR><TH>Name</TH><TH>Price</TH><TH>Team Size</TH></TR>";
 if($cart!="")
 {
 	$cart_array = explode(",",$cart);
@@ -26,6 +26,6 @@ if($cart!="")
 			echo "<TR><TD>$t[1]</TD><TD>$t[2]</TD><TD>$team_array[$i]</TD>";
 		}
 	}
-	echo"</TABLE><B>TOTAL = $sum<br>Demand Draft No: $dd</B><br>Pls Send the Demand Draft within 7 working days with your details to<br>VIT University,<br> Vellore- 632014, <br>Tamil Nadu.<a class='btn waves-effect waves-light blue-grey darken-4' href='event_list.php'>Home</a>";
+	echo"</TABLE><B>TOTAL = $sum<br>Demand Draft No: $dd</B><br>Pls Send the Demand Draft within 7 working days with your details to<br>VIT University,<br> Vellore- 632014, <br>Tamil Nadu.<br><a class='btn waves-effect waves-light indigo darken-4' href='event_list.php'><i class='material-icons left'>home</i>Home</a>";
 }
 ?>
