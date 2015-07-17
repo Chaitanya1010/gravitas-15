482d1ecf00a5ddb529422559078ae3fb7cf30179
shubhodeep9 <shubhodeep9@gmail.com>
2015-07-17 15:27:44 +0530

    Few tweaks

--- event_list.php
+++ event_list.php
@@ -215,12 +215,12 @@ function checkout()
 			<div class="row indigo darken-2" style="width:100%;padding-bottom:0.2em">
 			<div class="col s12">
 			  <ul class="tabs indigo darken-2">
-					<li class="tab col s2"><a href="#" class="white-text " id="type" name="type" value="0" onclick="search_events('body',this)">Premium</a></li>
-					<li class="tab col s2"><a href="#" class="white-text" id="type" name="type" value="1" onclick="search_events('body',this)">Workshops</a></li>
-					<li class="tab col s2"><a href="#" class="white-text" id="type" name="type" value="2" onclick="search_events('body',this)">Technical</a></li>
-					<li class="tab col s2"><a href="#" class="white-text" id="type" name="type" value="3" onclick="search_events('body',this)">Management</a></li>
-					<li class="tab col s2"><a href="#" class="white-text" id="type" name="type" value="4" onclick="search_events('body',this)">Informal</a></li>
-					<li class="tab col s2"><a href="#" class="white-text" id="type" name="type" value="5" onclick="search_events('body',this)">Combos</a></li>
+					<li class="tab col s2"><a href="#" class="white-text waves-effect" id="type" name="type" value="0" onclick="search_events('body',this)">Premium</a></li>
+					<li class="tab col s2"><a href="#" class="white-text waves-effect" id="type" name="type" value="1" onclick="search_events('body',this)">Workshops</a></li>
+					<li class="tab col s2"><a href="#" class="white-text waves-effect" id="type" name="type" value="2" onclick="search_events('body',this)">Technical</a></li>
+					<li class="tab col s2"><a href="#" class="white-text waves-effect" id="type" name="type" value="3" onclick="search_events('body',this)">Management</a></li>
+					<li class="tab col s2"><a href="#" class="white-text waves-effect" id="type" name="type" value="4" onclick="search_events('body',this)">Informal</a></li>
+					<li class="tab col s2"><a href="#" class="white-text waves-effect" id="type" name="type" value="5" onclick="search_events('body',this)">Combos</a></li>
 			  </ul>
 			</div>
 		  </div>
@@ -231,7 +231,7 @@ function checkout()
 				</div>&nbsp;
 				<ul class="collapsible popout col s6" style="float:right" data-collapsible="accordion">
 				<li>
-					<div class="collapsible-header indigo lighten-5 black-text"><i class="material-icons ">library_books</i><i class="material-icons right ">more_vert</i>Registered Events</div>
+					<div class="collapsible-header indigo lighten-5 black-text waves-effect"><i class="material-icons ">library_books</i><i class="material-icons right ">more_vert</i>Registered Events</div>
 					<div class="collapsible-body"><?php require("registered_events.php"); ?></div>
 				</li>
 				</ul>
@@ -241,7 +241,7 @@ function checkout()
 		</div>
 
 		<div class=" fixed-action-btn" style="bottom: 45px; right: 24px;">My Cart<br>
-			<a class="red btn-floating btn-large modal-trigger z-depth-3" title="Event Cart" href="#cart">
+			<a class="red btn-floating btn-large waves-effect modal-trigger z-depth-3" title="Event Cart" href="#cart">
 				<i class=" material-icons">shopping_cart</i>
 			</a>
 		</div>
