9bca4c3b4b34375b0d63fab5e92d8d4070876ca6
Rajalakshmi Senthil <shambhavi110@gmail.com>
2015-07-31 00:04:20 +0530

    mail

--- Externals/event_list.php
+++ Externals/event_list.php
@@ -355,13 +355,8 @@ $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
   });
-<<<<<<< HEAD
  </script>
  <script>
-=======
-</script>
-<script>
->>>>>>> origin/master
  $(document).ready(function() {
     $('select').material_select();
     $('.collapsible').collapsible({
@@ -375,12 +370,7 @@ $('.modal-trigger').leanModal();
     selectYears: 15 // Creates a dropdown of 15 years to control year
   });
 
-<<<<<<< HEAD
  </script>
-=======
-  });
-</script>
->>>>>>> origin/master
 </main>
 <footer class="page-footer indigo darken-4">
           <div class="footer-copyright">
